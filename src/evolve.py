520a375ab0686ba88ba6d8c4a9650db258055cd3
Alex Byrnes <alexbyrnes@gmail.com>
2022-05-04 10:26:20 -0400

    Evolve and evolve tests.

---
+++ src/evolve.py
@@ -0,0 +1,81 @@
+"""Module for simulating evolution of strings."""
+
+from random import choice, random
+from functools import partial
+
+def fitness(trial, target):
+    """Compare a string with a target. The more matching characters, the higher the fitness."""
+    return sum(t == h for t, h in zip(trial, target))
+
+def mutaterate(parent, target):
+    """Calculate a mutation rate that shrinks as the target approaches."""
+    perfectfitness = float(len(target))
+    return (perfectfitness - fitness(parent, target)) / perfectfitness
+
+def mutate(parent, rate, alphabet):
+    """Randomly change characters in parent to random characters from alphabet."""
+    return [(ch if random() <= 1 - rate else choice(alphabet)) for ch in parent]
+
+def mate(a, b):
+    """
+    Split two strings in the center and return 4 combinations of the two:
+        1. a
+        2. b
+        3. beginning of a, then the end of b
+        4. beginning of b, then the end of a
+    """
+    place = int(len(a)/2)
+    return a, b, b[:place] + a[place:], a[:place] + b[place:]
+
+
+def evolve(seed, target_string, alphabet, population_size=100):
+    """Randomly mutate populations of seed until it "evolves" into target_string."""
+
+    assert all([l in alphabet for l in target_string]), \
+            "Error: Target must only contain characters from alphabet."
+    assert len(seed) == len(target_string), \
+            "Error: Target and Seed must be the same length."
+
+    # For performance
+    target = list(target_string)
+    parent = seed
+    generations = 0
+
+    while parent != target:
+        rate = mutaterate(parent, target)
+        mutations = [mutate(parent, rate, alphabet) for _ in range(population_size)] + [parent]
+
+        center = int(population_size/2)
+        parent1 = max(mutations[:center], key=partial(fitness, target))
+        parent2 = max(mutations[center:], key=partial(fitness, target))
+        parent = max(mate(parent1, parent2), key=partial(fitness, target))
+        generations += 1
+    return generations
+
+print("Evolve some strings.")
+
+"""
+
+Classic example. From random seed to english sentence.
+
+"""
+seed = "RHBpoxYLCGjNpUgLYnMfiKskRHmk"
+target = "METHINKS IT IS LIKE A WEASEL"
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+
+generations = evolve(seed, target, alphabet)
+print(f"Success in {generations} generations!")
+
+
+"""
+
+DNA example.
+
+"""
+seed = "CGATGATGTATACTGTACGTATCTACTAC"
+dna_target = "AATCCGCTAGGTATCAGACTAGTAGCAGT"
+dna_alphabet = "ATCG"
+
+dna_generations = evolve(seed, dna_target, dna_alphabet)
+print(f"Success in {dna_generations} generations!")
+
---
+++ src/test_evolve.py
@@ -0,0 +1,35 @@
+import unittest
+from src.evolve import *
+
+class TestEvolveMethods(unittest.TestCase):
+
+    def test_fitness(self):
+        self.assertEqual(fitness("abcd", "axxd"), 2)
+        self.assertEqual(fitness("abcd", "abcd"), 4)
+        self.assertEqual(fitness("abcd", "wxyz"), 0)
+
+    def test_mutaterate(self):
+        self.assertEqual(mutaterate("abcd", "wxyz"), 1)
+        self.assertEqual(mutaterate("abcd", "abce"), 0.25)
+        self.assertEqual(mutaterate("abcd", "abcd"), 0)
+
+    def test_mutate(self):
+        """mutate with no mutation rate returns the parent."""
+        alphabet = "abcdefgh"
+        self.assertEqual(mutate("abcd", 0, alphabet), ['a', 'b', 'c', 'd'])
+
+        """mutate returns only characters from the alphabet."""
+        self.assertTrue(all([l in alphabet for l in mutate("abcd", 0.25, "abcdefgh")]))
+
+    def test_mate(self):
+        self.assertEqual(mate("abcd", "efgh"), ('abcd', 'efgh', 'abgh', 'efcd'))
+
+    def test_evolve(self):
+        """evolve raises error if target is not composed of the alphabet."""
+        with self.assertRaises(AssertionError):
+            evolve("abcd", "abcx", "abcd")
+
+        """evolve raises error if seed and target are different lengths."""
+        with self.assertRaises(AssertionError):
+            evolve("abc", "abcd", "abcd")
+
