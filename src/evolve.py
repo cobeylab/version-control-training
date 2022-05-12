9ef33f0d40e5e7fa3bc41e2e3d6a8ca7601dc710
Alex Byrnes <alexbyrnes@gmail.com>
2022-05-12 14:39:03 -0400

    Add comment

--- src/evolve.py
+++ src/evolve.py
@@ -53,6 +53,7 @@ def evolve(seed, target_string, alphabet, population_size=100):
     return generations
 
 
+# Command line parsing
 seed = sys.argv[1]
 target = sys.argv[2]
 alphabet = sys.argv[3]
