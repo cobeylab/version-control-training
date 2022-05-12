af6a1643e3035b7581c3e92b0353b9a82256c866
Alex Byrnes <alexbyrnes@gmail.com>
2022-05-12 14:42:11 -0400

    Add if name is main condition

--- src/evolve.py
+++ src/evolve.py
@@ -53,12 +53,13 @@ def evolve(seed, target_string, alphabet, population_size=100):
     return generations
 
 
-# Command line parsing
-seed = sys.argv[1]
-target = sys.argv[2]
-alphabet = sys.argv[3]
+if __name__ == "__main__":
+    # Command line parsing
+    seed = sys.argv[1]
+    target = sys.argv[2]
+    alphabet = sys.argv[3]
 
-generations = evolve(seed, target, alphabet)
+    generations = evolve(seed, target, alphabet)
 
-print(f"Success in {generations} generations!")
+    print(f"Success in {generations} generations!")
 
